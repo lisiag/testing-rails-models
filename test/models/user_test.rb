635af92615e2d3487047eba41139c32bfbcfc4dc
Lisia Grocott <lisiagrocott@gmail.com>
2022-03-08 19:21:38 +1300

    refactor tests to use fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Hermione', email: 'hm@hogwarts')
+    @user = users(:valid)
   end
 
   test 'valid user' do
