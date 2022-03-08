abf2f779172a783c1edc5939924956b81a1b6328
Lisia Grocott <lisiagrocott@gmail.com>
2022-03-08 19:03:14 +1300

    wip failing model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid user' do
+    user = User.new(name: 'Hermione', email: 'hm@hogwarts')
+    assert user.valid?
+  end
+
+  test 'invalid without name' do
+    user = User.new(email: 'someemail@someplace.com')
+    refute user.valid?, 'user without a name should be invalid'
+    assert_not_nil user.errors[:name], 'user without a name should trigger validation error'
+  end
+
+  test 'invalid without email' do
+    user = User.new(name: 'Lucy')
+    refute user.valid?, 'user without an email should be invalid'
+    assert_not_nil user.errors[:email], 'user without an email should trigger validation error'
+  end
 end
