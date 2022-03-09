e1cdd059100a00fcc3009b3ff5f7ef0c6b88280f
Lisia Grocott <lisiagrocott@gmail.com>
2022-03-09 13:46:13 +1300

    refactor profile photo url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
   scope :recent, -> { where('created_at > ?', 1.week.ago) }
+
+  PROFILE_PHOTO_ROOT_URL = URI "https://s.gravatar.com/avatar/"
+
   has_many :posts
   validates :name, :email, presence: true
 
   def profile_photo_url
-    "https://s.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
+    url = PROFILE_PHOTO_ROOT_URL.clone
+    url.path << email_md5
+    url.to_s
+  end
+
+  def email_md5
+    Digest::MD5.hexdigest email
   end
 end
